9505f4db14998bdb45f92bf981f46e57b0f0578b
andela-gike <grace.ike@andela.com>
2017-06-22 19:12:53 +0100

    Resolve error message from eslint server

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Todo from '@/components/TodoList'
+import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Todo',
-      component: Todo
+      name: 'Hello',
+      component: Hello
     }
   ]
 })
